7df8d65ad17de8c79c53af3b54be6855b3a8678b
Latricia <tricianickelberry@gmail.com>
2021-01-07 21:46:52 -0800

    added action container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import RestaurantsContainer from './containers/RestaurantsContainer'
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
-        App
+        <RestaurantsContainer/>
       </div>
     );
   }
---
+++ src/containers/RestaurantsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RestaurantsContainer extends Component {
+    render() {
+        return (
+            <div>
+               Restaurant Container 
+            </div>
+        )
+    }
+}
