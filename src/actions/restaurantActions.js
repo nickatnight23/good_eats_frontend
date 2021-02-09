6fe33ae61a37d14b70b6407886ff962db860bd73
Latricia <tricianickelberry@gmail.com>
2021-02-09 08:52:13 -0800

    updated actions

--- src/actions/restaurantActions.js
+++ src/actions/restaurantActions.js
@@ -2,17 +2,22 @@
 
 export const fetchRestaurant = () =>{
 
+    // dispatch function receives the dispatch method from the store
+
     return (dispatch) => {
-        fetch('http://localhost:3001/api/v1/restaurants')
-        .then(resp => resp.json())
+        fetch('http://localhost:3001/api/v1/restaurants')  // async request is made 
+        .then(resp => resp.json()) //promise
         .then(restaurants =>{
             dispatch({type: 'FETCH_RESTAURANTS', payload: restaurants})})
     }
 }
 
+// once the promise is resolved the fetch thunk takes the response.restaurants array that was returned
+// from the callback and dispatches an action containing the resturants array as action.payload
 
+// action creator.... Thunk action creator
 export const addRestaurant = restaurant => {
-    
+    // Return an Action code that causes an update to the state when something happens
     return (dispatch) => {
         fetch('http://localhost:3001/api/v1/restaurants',{
         
