62b33054dc559fcfd1d4206586979bcc0a0e8be9
Latricia <tricianickelberry@gmail.com>
2021-05-16 16:28:20 -0700

    updated home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div>
            <b><p></p></b>
-            <h1>Welcome to Good Eats</h1>
+            <h1>Hi, welcome to good eats/h1>
               {/* <b><p></p></b> */}
                <img src={restaurant} alt="restaurant" height={500} width={500}/>
             <p>I LOVE FOOD</p>
